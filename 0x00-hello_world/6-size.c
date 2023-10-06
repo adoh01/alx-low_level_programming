bfe24fe115996c413b347802425d80806a7b28a7
adoh01 <godswilladoh@gmail.com>
2023-10-05 21:14:47 -0700

    C auto commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main - name of the primary function where the program execution starts
+ * Return: exit a function and optionally return a value to the calling code
+ */
+
+int main(void)
+{
+	
+}
