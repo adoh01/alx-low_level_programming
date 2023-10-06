3126cdd699655c22567c091c0a4959769c80e108
adoh01 <godswilladoh@gmail.com>
2023-10-05 20:48:06 -0700

    C auto commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - serves as the global function
+ *
+ * Return: ends command and is 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
