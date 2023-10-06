0c3e28515a066d389f544e948716f0341cee8454
adoh01 <godswilladoh@gmail.com>
2023-10-05 21:51:37 -0700

    C auto commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
-/*
- * main - name of the primary function where the program execution starts
+/**
+ * main - calculates size of data types in bytes
+ *
  * Return: exit a function and optionally return a value to the calling code
  */
 
 int main(void)
 {
-	
+	printf("Size of char: %lu byte(s)", sizeof(char));
+	printf("Size of int: %lu byte(s)", sizeof(int));
+	printf("Size of long int: %lu byte(s)", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of float: %lu byte(s)", sizeof(float));
+	return (0);
 }
