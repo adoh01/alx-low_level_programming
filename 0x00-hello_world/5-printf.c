3505906c63e4a18cf5fc5a0fe7a9d0e3338d9587
adoh01 <godswilladoh@gmail.com>
2023-10-05 21:10:00 -0700

    C auto commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the name of the primary function where the program execution starts.
+ * Return: exit a function and optionally return a value to the calling code
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
