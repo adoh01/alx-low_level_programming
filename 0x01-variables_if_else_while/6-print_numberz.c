eee43363788fc04021ab61a09ce522e2d827b9f3
adoh01 <godswilladoh@gmail.com>
2023-10-11 10:15:31 -0700

    C auto commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
