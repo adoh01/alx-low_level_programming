fd4cd1c4ee4dc20e50e7e8ced129805b6b3aea04
adoh01 <godswilladoh@gmail.com>
2023-10-13 00:31:48 -0700

    C auto commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,13 +9,9 @@ int main(void)
 {
 	int n1, n2;
 
-	n1 = 0;
-
-	while (n1 <= 98)
+	for (n1 = 0; n1 < 99; n1++)
 	{
-		n2 = 1;
-
-		while (n2 <= 99)
+		for (n2 = n1 + 1; n2 <= 99; n2++)
 		{
 			putchar((n1 / 10) + '0');
 			putchar((n1 % 10) + '0');
