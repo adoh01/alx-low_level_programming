c2c3cb34bc8be960787112f4cec3700ca9bd0684
adoh01 <godswilladoh@gmail.com>
2023-10-13 10:36:19 -0700

    C auto commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lower characters
+ * @c: The character to be shecked
+ * Return: 1 for lower characters or 0 for anything else
+ */
+
+int _islower(int c)
+
+{
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+}
